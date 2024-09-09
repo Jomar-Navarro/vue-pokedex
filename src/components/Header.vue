74732b061cea4bc32e82214ea3210fc14c09182a
Jomar Navarro <145290826+Jomar-Navarro@users.noreply.github.com>
2024-09-09 20:40:50 +0200

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script>  
+  import { store } from '../data/store'
+  export default {
+    data(){
+      return{
+        store,
+      }
+    }
+  }
+</script>
+
+
+<template>
+  <div class="text-center">
+    Header
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
